4bca862be081e9cb21d5517f7b6a05e26e5cf7d4
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-18 14:20:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,6 @@ def divider
   "-----------\n"
 end
 def display_board
-# puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
 board = bars + divider + bars + divider + bars
 puts board
 end
