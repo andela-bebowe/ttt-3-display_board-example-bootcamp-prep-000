a5425956c32060f5266706d49bbe81d182ea414b
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-18 14:09:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,13 @@
+def bars
+  "   |   |   "
+end
 
+def divider
+  "-----------"
+end
 def display_board
-puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
+# puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
+board = bars + dividider + bars + divider + bars
 end
 
 display_board
